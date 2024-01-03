95be787fce52b11e2adce6487a9116404fa3685e
Jérémy Bézairie <109919050+jebez1@users.noreply.github.com>
2024-01-03 11:38:09 +0000

    Add files via upload

---
+++ video no-fit &  toggle fullscreen pointing on top/!.js
@@ -0,0 +1,6 @@
+addEventListener("click",$,{once:true})
+function $(){document.documentElement.requestFullscreen()
+A=new AbortController()
+addEventListener("mousemove",e=>{if(e.y==0){document.exitFullscreen()
+A.abort()
+addEventListener("click",$,{once:true})}},{signal:A.signal})}
